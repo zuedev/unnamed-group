ce8d26dac4d8a3ba6f239f915d4336b2a4c35ed4
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-05-12 22:26:55 +0100

    only show auth header when auth query param is present

--- communities/red-right-hand/174bg.net/src/app/layout.js
+++ communities/red-right-hand/174bg.net/src/app/layout.js
@@ -1,13 +1,9 @@
-import {
-  ClerkProvider,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Fira_Code } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import TerminalOverlay from "@/components/TerminalOverlay";
+import AuthHeader from "@/components/AuthHeader";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -35,19 +31,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <ClerkProvider>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
-          </header>
+          <AuthHeader />
           <TerminalOverlay />
           {children}
         </ClerkProvider>
