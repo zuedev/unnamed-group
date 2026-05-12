ffe99a7490325366e230786c8bb89616c1a2dcb4
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-05-12 22:34:26 +0100

    useSearchParams() should be wrapped in a suspense boundary

--- communities/red-right-hand/174bg.net/src/app/layout.js
+++ communities/red-right-hand/174bg.net/src/app/layout.js
@@ -31,7 +31,9 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <ClerkProvider>
-          <AuthHeader />
+          <Suspense>
+            <AuthHeader />
+          </Suspense>
           <TerminalOverlay />
           {children}
         </ClerkProvider>
