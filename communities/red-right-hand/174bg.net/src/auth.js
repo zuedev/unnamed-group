005a92d247e42d56a4f950f1f8c730686e6e5a88
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-05-13 16:58:47 +0100

    add env to better auth config

--- communities/red-right-hand/174bg.net/src/auth.js
+++ communities/red-right-hand/174bg.net/src/auth.js
@@ -8,6 +8,8 @@ const postgresPort = process.env.POSTGRES_PORT || 5432;
 const postgresDatabase = process.env.POSTGRES_DB || "postgres";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   database: new Pool({
     connectionString: `postgresql://${postgresUser}:${postgresPassword}@${postgresHost}:${postgresPort}/${postgresDatabase}`,
   }),
