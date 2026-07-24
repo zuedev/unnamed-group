5cae0bed9d2504f1c644ee6f3a27e570f9ceeb03
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-07-24 20:00:01 +0100

    typo

--- 174bg/database/deploy.ps1
+++ 174bg/database/deploy.ps1
@@ -2,7 +2,7 @@ $ROOT = "/docker-compose/db.174bg.net/"
 
 ssh "root@docker-compose" "cd $ROOT && docker compose down && rm -rf ./volumes/pocketbase/pb_hooks/*"
 
-scp -r ".\volumes\pocketbase\pb_hooks\" "root@docker-compose:$ROOT/volumes/pocketbase/pb_hooks/"
+scp -r ".\volumes\pocketbase\pb_hooks\" "root@docker-compose:$ROOT/volumes/pocketbase/"
 
 scp ".\docker-compose.yaml" "root@docker-compose:$ROOT/docker-compose.yaml"
 
