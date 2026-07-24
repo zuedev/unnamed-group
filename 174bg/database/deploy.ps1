8ce0de9b388e67890f7d495785c715dd200d9afb
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-07-24 19:49:13 +0100

    update deploy script

--- 174bg/database/deploy.ps1
+++ 174bg/database/deploy.ps1
@@ -1,3 +1,7 @@
-$ROOT = "/docker-compose/db.174bg.net/";
+$ROOT = "/docker-compose/db.174bg.net/"
 
 scp ".\volumes\pocketbase\pb_hooks\main.pb.js" "root@docker-compose:$ROOT/volumes/pocketbase/pb_hooks/main.pb.js"
+
+scp ".\docker-compose.yaml" "root@docker-compose:$ROOT/docker-compose.yaml"
+
+ssh "root@docker-compose" "cd $ROOT && docker compose up -d --build --remove-orphans"
