18340bb1606a7fe4b74e5ff888fd2d60af9e1d8d
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-07-24 19:53:01 +0100

    clear pb_hooks dir prior to update

--- 174bg/database/deploy.ps1
+++ 174bg/database/deploy.ps1
@@ -1,6 +1,8 @@
 $ROOT = "/docker-compose/db.174bg.net/"
 
-scp ".\volumes\pocketbase\pb_hooks\main.pb.js" "root@docker-compose:$ROOT/volumes/pocketbase/pb_hooks/main.pb.js"
+ssh "root@docker-compose" "cd $ROOT && docker compose down && rm -rf ./volumes/pocketbase/pb_hooks/*"
+
+scp -r ".\volumes\pocketbase\pb_hooks\" "root@docker-compose:$ROOT/volumes/pocketbase/pb_hooks/"
 
 scp ".\docker-compose.yaml" "root@docker-compose:$ROOT/docker-compose.yaml"
 
