a3665d47611c398169144ecdd6158bbfb0a2b8bd
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-07-28 20:31:13 +0100

    oops

--- 174bg/website-nextjs/.next/dev/static/chunks/16fj_next_dist_pages__error_0h3z60u.js
+++
@@ -1,8 +0,0 @@
-(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/174bg/website-nextjs/node_modules/next/dist/pages/_error.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-const e = new Error("Could not parse module '[project]/174bg/website-nextjs/node_modules/next/dist/pages/_error.js', file not found");
-e.code = 'MODULE_UNPARSABLE';
-throw e;
-}),
-]);
--- 174bg/website-nextjs/.next/dev/static/chunks/[project]_174bg_website-nextjs_pages__error_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/026__next_dist_compiled_02f3z21._.js","static/chunks/026__next_dist_shared_lib_1i4sc9d._.js","static/chunks/026__next_dist_client_01fxku6._.js","static/chunks/026__next_dist_19u0nw9._.js","static/chunks/16fj_next_dist_pages__error_0h3z60u.js","static/chunks/[next]_entry_page-loader_ts_1y1trrg._.js","static/chunks/026__react-dom_1hn1uh9._.js","static/chunks/026__1enaj_i._.js","static/chunks/[root-of-the-server]__0ertkhr._.js"],
-    source: "entry"
-});
