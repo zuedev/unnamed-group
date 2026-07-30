d3fc14f4ef3498f1c38aae0a2d8d3e5a4a3cf79e
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-07-30 08:16:46 +0100

    redirs

--- 174bg/website/next.config.mjs
+++ 174bg/website/next.config.mjs
@@ -2,7 +2,48 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
   output: "standalone",
+
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/RGgSfVj4DD",
+        permanent: true,
+      },
+      {
+        source: "/handbook",
+        destination: "https://handbook.174bg.net",
+        permanent: true,
+      },
+      {
+        source: "/docs",
+        destination: "https://handbook.174bg.net",
+        permanent: true,
+      },
+      {
+        source: "/manager",
+        destination: "https://manager.174bg.net",
+        permanent: true,
+      },
+      {
+        source: "/login",
+        destination: "https://manager.174bg.net",
+        permanent: true,
+      },
+      {
+        source: "/database",
+        destination: "https://db.174bg.net",
+        permanent: true,
+      },
+      {
+        source: "/db",
+        destination: "https://db.174bg.net",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
