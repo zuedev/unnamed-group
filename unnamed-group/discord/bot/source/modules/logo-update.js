6df16b95f877b79a2bd4574267ad7707b84de182
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-08-15 14:53:19 +0100

    refactor logo update interval to every 10 minutes

--- unnamed-group/discord/bot/source/modules/logo-update.js
+++ unnamed-group/discord/bot/source/modules/logo-update.js
@@ -1,14 +1,18 @@
 export default (discord) => {
-  setInterval(async () => {
-    const logoUrls = [
-      "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/official.png?raw=true",
-      "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/153172054856826880.png?raw=true",
-      "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/723361818940276736.png?raw=true",
-    ];
+  setInterval(
+    async () => {
+      const logoUrls = [
+        "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/official.png?raw=true",
+        "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/153172054856826880.png?raw=true",
+        "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/723361818940276736.png?raw=true",
+      ];
 
-    const randomLogoUrl = logoUrls[Math.floor(Math.random() * logoUrls.length)];
+      const randomLogoUrl =
+        logoUrls[Math.floor(Math.random() * logoUrls.length)];
 
-    const guild = await discord.guilds.fetch(process.env.DISCORD_GUILD_ID);
-    await guild.setIcon(randomLogoUrl, "bot logo update");
-  }, 1000 * 60);
+      const guild = await discord.guilds.fetch(process.env.DISCORD_GUILD_ID);
+      await guild.setIcon(randomLogoUrl, "bot logo update");
+    },
+    1000 * 60 * 10, // every 10 minutes
+  );
 };
