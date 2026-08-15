4a3567e5e19ba28cfc39613e011778394a2a913e
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-08-15 15:27:28 +0100

    remove console logs

--- unnamed-group/discord/bot/source/modules/voice-chats.js
+++ unnamed-group/discord/bot/source/modules/voice-chats.js
@@ -24,45 +24,17 @@ export function voiceChats(discord) {
     // ignore events from other guilds not specified in the .env file
     if (newState.guild.id !== process.env.DISCORD_GUILD_ID) return;
 
-    console.log(`Voice state updated:`, {
-      user: {
-        id: newState.member.id,
-        username: newState.member.user.username,
-        discriminator: newState.member.user.discriminator,
-      },
-      oldChannel: {
-        id: oldState.channelId,
-        name: oldState.channel?.name,
-      },
-      newChannel: {
-        id: newState.channelId,
-        name: newState.channel?.name,
-      },
-    });
-
     // has the user joined the "create voice channel" voice channel?
     if (newState.channel?.name === "create voice channel") {
-      console.log(
-        `User ${newState.member.user.username} has joined the "create voice channel" voice channel. Creating a new voice channel...`,
-      );
-
       // does the user already have a voice channel?
       if (
-        newState.guild.channels.cache.some((channel) => {
+        !newState.guild.channels.cache.some((channel) => {
           return (
             channel.type === ChannelType.GuildVoice &&
             channel.name === `${newState.member.user.username}'s Voice Channel`
           );
         })
       ) {
-        console.log(
-          `User ${newState.member.user.username} already has a voice channel. Not creating a new one.`,
-        );
-      } else {
-        console.log(
-          `User ${newState.member.user.username} does not have a voice channel. Creating a new one.`,
-        );
-
         const guild = newState.guild;
         const newChannel = await guild.channels.create({
           name: `${newState.member.user.username}'s Voice Channel`,
@@ -75,8 +47,6 @@ export function voiceChats(discord) {
             },
           ],
         });
-
-        console.log(`Created new voice channel: ${newChannel.name}`);
       }
 
       // get the id of the new (or not new) voice channel
@@ -89,10 +59,6 @@ export function voiceChats(discord) {
 
       // move the user to the new voice channel
       await newState.member.voice.setChannel(voiceChannelId);
-
-      console.log(
-        `Moved user ${newState.member.user.username} to their voice channel.`,
-      );
     }
   });
 }
