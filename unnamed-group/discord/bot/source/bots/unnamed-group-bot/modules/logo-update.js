d826504383d007d9e3a719fbdbac90cb2cbeb110
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-08-21 23:48:56 +0100

    easier member logo resolution

--- unnamed-group/discord/bot/source/bots/unnamed-group-bot/modules/logo-update.js
+++ unnamed-group/discord/bot/source/bots/unnamed-group-bot/modules/logo-update.js
@@ -3,10 +3,15 @@ export function logoUpdate(discord) {
     async () => {
       const logoUrls = [
         "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/official.png?raw=true",
-        "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/153172054856826880.png?raw=true",
-        "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/723361818940276736.png?raw=true",
-        "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/247625024779845632.png?raw=true",
-        "https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/313128528558161920.png?raw=true",
+        ...[
+          "153172054856826880.png",
+          "723361818940276736.png",
+          "247625024779845632.png",
+          "313128528558161920.png",
+        ].map(
+          (filename) =>
+            `https://github.com/zuedev/unnamed-group/blob/master/unnamed-group/discord/assets/images/logo/member-logos/${filename}?raw=true`,
+        ),
       ];
 
       const randomLogoUrl =
