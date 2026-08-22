f9566f0c9aa727286170bafc1311884706c30ae8
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-08-22 23:25:59 +0100

    sort servers array by server name

--- unnamed-group/servers.unnamed.group/source/main.js
+++ unnamed-group/servers.unnamed.group/source/main.js
@@ -83,7 +83,9 @@ const servers = [
       },
     },
   },
-].sort((a, b) => a.gameName.localeCompare(b.gameName));
+]
+  .sort((a, b) => a.gameName.localeCompare(b.gameName))
+  .sort((a, b) => a.serverName.localeCompare(b.serverName));
 
 Deno.serve(
   { port: Number(Deno.env.get("HTTP_PORT") || 80), hostname: "0.0.0.0" },
